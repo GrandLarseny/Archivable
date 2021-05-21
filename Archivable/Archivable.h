2ae4f95aff9dc9199bb64a033c5468db917e68a8
Daniel Larsen <daniel@daniellarsen.com>
2021-05-20 22:30:02 -0500

    Initial Commit

---
+++ Archivable/Archivable.h
@@ -0,0 +1,18 @@
+//
+//  Archivable.h
+//  Archivable
+//
+//  Created by Daniel Larsen on 5/20/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Archivable.
+FOUNDATION_EXPORT double ArchivableVersionNumber;
+
+//! Project version string for Archivable.
+FOUNDATION_EXPORT const unsigned char ArchivableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Archivable/PublicHeader.h>
+
+
